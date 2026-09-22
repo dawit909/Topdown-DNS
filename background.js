a16c214cb3e3baba056f37779102e6c82da87f4c
Dawit Jung <zshivan@proton.me>
2026-09-23 08:01:33 +0900

    Fixed it trying to treat clicked urls as Topdown when it starts with TLD(e.g. ru.wikipedia.org)

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 // top 338 by traffic
 const TLDs = ["com", "net", "org", "jp", "de", "uk", "fr", "br", "it", "ru", "es", "me", "gov", "pl", "ca", "au", "cn", "co", "in", "nl", "edu", "info", "eu", "ch", "id", "at", "kr", "cz", "mx", "be", "tv", "se", "tr", "tw", "al", "ua", "ir", "vn", "cl", "sk", "ly", "cc", "to", "no", "fi", "us", "pt", "dk", "ar", "hu", "tk", "gr", "il", "news", "ro", "my", "biz", "ie", "za", "nz", "sg", "ee", "th", "io", "xyz", "pe", "bg", "hk", "rs", "lt", "link", "ph", "club", "si", "site", "mobi", "by", "cat", "wiki", "la", "ga", "xxx", "cf", "hr", "ng", "jobs", "online", "kz", "ug", "gq", "ae", "is", "lv", "pro", "fm", "tips", "ms", "sa", "app", "lat", "pk", "ws", "top", "xn", "pw", "ai", "kw", "ml", "su", "lu", "nu", "ec", "uy", "az", "ma", "st", "asia", "im", "am", "email", "ke", "eg", "live", "md", "uz", "today", "ge", "ba", "bo", "blog", "lk", "do", "one", "ve", "media", "sh", "vip", "life", "guru", "mp", "int", "tz", "desi", "jo", "mk", "np", "py", "mu", "mn", "re", "travel", "tn", "gd", "sn", "cam", "shop", "mil", "tokyo", "gg", "kg", "cr", "li", "cu", "hn", "eus", "rocks", "bd", "dev", "fun", "video", "gt", "bz", "space", "store", "mt", "name", "work", "porn", "pub", "kh", "icu", "as", "cy", "pr", "careers", "ps", "dz", "iq", "ltd", "pa", "sv", "qa", "design", "sc", "sy", "aero", "tech", "best", "scot", "win", "ni", "bzh", "lb", "city", "digital", "red", "pg", "world", "ag", "ci", "report", "ac", "center", "ad", "ovh", "om", "cloud", "press", "zw", "vg", "cx", "website", "plus", "page", "bs", "cd", "gh", "moe", "help", "download", "ao", "cm", "af", "click", "mg", "tm", "na", "pics", "mv", "market", "rw", "fo", "fj", "tl", "gratis", "art", "mc", "mo", "gal", "coop", "nc", "run", "mz", "tj", "zone", "cv", "bh", "place", "gy", "agency", "kn", "mm", "bid", "bj", "je", "gs", "global", "lol", "tube", "lc", "bm", "ninja", "bn", "review", "bi", "ht", "sm", "ax", "pm", "ink", "vc", "so", "tel", "cw", "uno", "casa", "network", "buzz", "mw", "vu", "guide", "bf", "mq", "sex", "sr", "zm", "onl", "vet", "bw", "jm", "bar", "gp", "pf", "ne", "va", "bt", "sd", "expert", "kim", "africa", "mr", "gi", "tg", "cg", "ls", "sb", "ck", "gf", "yt", "dm", "realtor", "aw", "sz", "london", "et", "ky", "cool", "lr", "bb", "support", "taipei", "sl", "company", "dj", "social", "gm", "xn", "love"];
 
+// top 3 by traffic
+const superPrimaryTLDs = ["com", "net", "org"]
+
 // top 30 by traffic
 const primaryTLDs = ["com", "net", "org", "jp", "de", "uk", "fr", "br", "it", "ru", "es", "me", "gov", "pl", "ca", "au", "cn", "co", "in", "nl", "edu", "info", "eu", "ch", "id"];
 
@@ -11,7 +14,6 @@ const shortcuts = {
     "o": "org",
     "g": "gov",
     "e": "edu",
-
 };
 
 // Map of common search engines to catch queries
@@ -72,16 +74,18 @@ function expandCustomMounts(domainStr) {
 
 function isTopDownDomain(domainStr) {
     if (domainStr.includes(' ')) return false;
+    if (domainStr === ".") return false
 
     let parts = domainStr.split('.');
-    if (parts.length < 2) return false;
 
     let firstPart = parts[0].toLowerCase();
     let lastPart = parts[parts.length - 1].toLowerCase();
 
     if (shortcuts[firstPart] !== undefined) return true;
+
+    if (superPrimaryTLDs.includes(lastPart)) return false;
     if (primaryTLDs.includes(firstPart)) return true;
-    if (TLDs.includes(firstPart) && !primaryTLDs.includes(lastPart) && !TLDs.includes(lastPart)) return true;
+    if (TLDs.includes(firstPart) && !TLDs.includes(lastPart)) return true;
 
     return false;
 }
@@ -99,6 +103,12 @@ function reverseDomain(domainStr) {
 browser.webRequest.onBeforeRequest.addListener(
     function (details) {
         if (details.type !== "main_frame") return;
+
+        // ORIGIN FIX: Ignore requests that originate from clicking a link on a webpage.
+        // Typed URLs will either lack an originUrl or use internal schemas (like about:newtab).
+        if (details.originUrl && details.originUrl.startsWith("http")) {
+            return;
+        }
 
         let url = new URL(details.url);
 
